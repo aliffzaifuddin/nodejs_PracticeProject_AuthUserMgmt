52c77dfde913873ff62d3ed28962e4bd6bc7dc6e
Aliff Zaifuddin <aliffzaifuddin97@gmail.com>
2024-01-09 10:10:45 +0000

    upload new project from hands on lab assignment in Coursera

--- index.js
+++ index.js
@@ -33,40 +33,46 @@ app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
 
 app.use(express.json());
 
+/* This below code ensures that all the endpoints starting with /friends go through the middleware. It retrieves the authorization details from the session and verifies it. If the token is validated, the user is aunteticated and the control is passed on to the next endpoint handler. If the token is invalid, the user is not authenticated and an error message is returned. */
+
 app.use("/friends", function auth(req,res,next){
-   if(req.session.authorization) {
-       token = req.session.authorization['accessToken'];
-       jwt.verify(token, "access",(err,user)=>{
-           if(!err){
-               req.user = user;
-               next();
-           }
-           else{
-               return res.status(403).json({message: "User not authenticated"})
-           }
-        });
-    } else {
-        return res.status(403).json({message: "User not logged in"})
-    }
+  if(req.session.authorization) {
+    token = req.session.authorization['accessToken'];
+    jwt.verify(token, "access",(err,user)=>{
+      if(!err){
+        req.user = user;
+        next();
+      }
+      else{
+        return res.status(403).json({message: "User not authenticated"})
+      }
+    });
+  } else {
+    return res.status(403).json({message: "User not logged in"})
+  }
 });
+
+// Create an endpoint that will do the following
+// Return an error if the username or password is not provided.
+// Creates an access token that is valid for 1 hour (60 X 60 seconds) and logs the user in, if the credentials are correct.
+// Throws an error, if the credentials are incorrect.
 
 app.post("/login", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
 
   if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
+    return res.status(404).json({message: "Error logging in"});
   }
 
   if (authenticatedUser(username,password)) {
     let accessToken = jwt.sign({
       data: password
     }, 'access', { expiresIn: 60 * 60 });
-
     req.session.authorization = {
       accessToken,username
-  }
-  return res.status(200).send("User successfully logged in");
+    }
+    return res.status(200).send("User successfully logged in");
   } else {
     return res.status(208).json({message: "Invalid Login. Check username and password"});
   }
@@ -93,3 +99,5 @@ const PORT =5000;
 app.use("/friends", routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
+
+// You have an express server that has been configured to run at port 5000. When you access the server with /friends, you can access the end points defined in routes/friends.js. But for doing this, you need to register as a new user in the /register endpoint and login with those credentials in the /login endpoint.
